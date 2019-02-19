8760b8e8ec13fcc01c6863d7e6f2dc928d7fd405
Luis Salazar <LuisMBA@Luiss-MacBook-Air.local>
2019-02-18 21:43:29 -0500

    javascript for navbar across all pages

---
+++ navscript.js
@@ -0,0 +1,8 @@
+ document.getElementById("NavMenu").innerHTML =
+ 	'<ul id="navbar">'+
+	  '<li><a href="index.html">Home</a></li>'+
+	  '<li><a href="demo.html">Demo</a></li>'+
+	  '<li><a href="api.html">API</a></li>'+
+	  '<li><a href="">App</a></li>'+
+	  '<li><a href="">About</a></li>'+
+  	'</ul>';
